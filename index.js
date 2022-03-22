d11287dcc9928783287b4488b5a15ec79d1ace5a
Monique Lena Moore <moniquelmoore06@gmail.com>
2022-03-22 01:09:15 -0400

    Halfway Point

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+// Code your solutions in this file==below is "for" loop
+/*for (let age = 30; age < 40; age++){ 
+    console.log(`I'm ${age} years old. Happy birthday to me!`)
+/*debugger}*/
+
+//Using for with Arrays-The for loop is often used to iterate over every element in an array. 
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    //debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
